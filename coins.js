fb1dba1ffc6a2b2bf6b84840e67efb5758271c58
kagurakana <1278820830@qq.com>
2021-01-27 21:32:54 +0800

    ✨feat(动态规划): 最少硬币问题

--- coins.js
+++ coins.js
@@ -6,18 +6,41 @@
  * https://github.com/labuladong/fucking-algorithm/blob/master/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E8%AF%A6%E8%A7%A3%E8%BF%9B%E9%98%B6.md#%E4%BA%8C%E5%87%91%E9%9B%B6%E9%92%B1%E9%97%AE%E9%A2%98
  */
 
+// 自顶向下，带备忘录
 function coinChange(coins, amount) {
+  let arr = []
+
   function dp(n) {
     if ((n === 0)) return 0;
     if (n < 0) return -1;
+    //查备忘录，避免重复计算
+    if (arr[n] !== undefined) return arr[n]
     let res = Infinity;
     for (coin of coins) {
       let subproblem = dp(n - coin); // 求解子问题
       res = subproblem === -1 ? res : Math.min(res, subproblem + 1);
     }
-    return res === Infinity ? -1 : res;
+    //记入备忘录
+    arr[n] = res === Infinity ? -1 : res
+    return arr[n]
   }
   return dp(amount);
 }
 
-console.log(coinChange([1, 2, 5], 11));
+// console.log(coinChange([1, 2, 5], 11));
+
+// 自底向上，dp数组的迭代解法
+function coinChange2(coins, amount) {
+  //dp[]共i项,i表示目标金额，dp[i]表示凑出i元需要的硬币数量 
+  let dp = [0]
+  for (let i = 0; i <= amount; i++) {
+    dp.push(amount + 1) //这里dp[]会多出1项,不过最后返回dp[amount] 即可，多出的一项不用管
+    for (coin of coins) {
+      dp[i] = i - coin < 0 ? dp[i] : Math.min(dp[i], dp[i - coin] + 1)
+    }
+  }
+  return dp[amount]
+}
+//凑成 amount 金额的硬币数最多只可能等于 amount（全用 1 元面值的硬币），所以初始化为 amount + 1 就相当于初始化为正无穷，便于后续取最小值
+
+console.log(coinChange2([1, 2, 5], 12))
